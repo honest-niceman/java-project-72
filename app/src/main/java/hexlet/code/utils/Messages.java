72d52ca4e0224f63c75bc051151561062a4d5967
Georgii Vlasov <honest.niceman@gmail.com>
2023-07-21 10:49:23 +0400

    Russiafication

--- app/src/main/java/hexlet/code/utils/Messages.java
+++ app/src/main/java/hexlet/code/utils/Messages.java
@@ -1,8 +1,8 @@
 package hexlet.code.utils;
 
 public class Messages {
-    public static final String SUCCESS = "Page successfully added (Страница успешно добавлена).";
-    public static final String FOUND = "Page already exists.";
-    public static final String INCORRECT_URL = "Incorrect URL.";
-    public static final String CHECK_SUCCESS = "Page successfully checked (Страница успешно проверена).";
+    public static final String SUCCESS = "Страница успешно добавлена.";
+    public static final String FOUND = "Страница уже существует.";
+    public static final String INCORRECT_URL = "Некорректный URL.";
+    public static final String CHECK_SUCCESS = "Страница успешно проверена.";
 }
