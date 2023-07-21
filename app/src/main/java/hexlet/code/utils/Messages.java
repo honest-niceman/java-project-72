338ca817d172a9e26b304fe3f56fce27e0481f58
Georgii Vlasov <honest.niceman@gmail.com>
2023-07-21 10:42:29 +0400

    Adjustments to pass checks.

--- app/src/main/java/hexlet/code/utils/Messages.java
+++ app/src/main/java/hexlet/code/utils/Messages.java
@@ -1,8 +1,8 @@
 package hexlet.code.utils;
 
 public class Messages {
-    public static final String SUCCESS = "Page successfully added.";
+    public static final String SUCCESS = "Page successfully added (Страница успешно добавлена).";
     public static final String FOUND = "Page already exists.";
     public static final String INCORRECT_URL = "Incorrect URL.";
-    public static final String CHECK_SUCCESS = "Page successfully checked.";
+    public static final String CHECK_SUCCESS = "Page successfully checked (Страница успешно проверена).";
 }
